f6a29fee642e49aa1c65a8074bf1c49de0652089
beanonlooker <3509769208@qq.com>
2023-08-26 00:14:39 +0800

    feat:一个测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,29 @@
 <template>
-helloWorld
+
+<ul>
+    <li v-for="list in lists" :key="list.id">
+        <span>{{ list.name }}</span>
+        <input type="text">
+    </li>
+</ul>
+<button @click="add">增加</button>
+
 </template>
 
 <script setup lang='ts'>
+import { reactive } from 'vue';
+
+const lists = reactive([
+    {id:1,name:'张三'},
+    {id:2,name:'李四'},
+    {id:3,name:'王五'},
+])
+
+const add = () => {
+    lists.unshift({id:0,name:'张er'})
+    console.log(lists);
+    
+}
 
 </script>
 
