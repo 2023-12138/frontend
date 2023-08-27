fd05ae203aad200178e5251f3a9ba02c14f01050
beanonlooker <3509769208@qq.com>
2023-08-27 16:28:47 +0800

    feat:新增markdown编辑器

--- src/pages/Team/Doc.vue
+++ src/pages/Team/Doc.vue
@@ -1,12 +1,34 @@
 <template>
-<div id="main">
-
-</div>
+    <div class="container">
+        <div class="header">
+            <div class="headerContainer">
+                <div class="headerLeft">
+                    <div class="back">
+                        <n-icon size="29" :component="ArrowBackSharp" />
+                    </div>
+                    <div class="title">
+                        文档名
+                    </div>
+                    <div class="editTitle">
+                        <n-icon size="17" :component="Edit16Filled" />
+                    </div>
+                </div>
+                <div class="headerRight">
+                    <n-button>导出</n-button>
+                    <n-button>保存</n-button>
+                </div>
+            </div>
+        </div>
+        <div id="main"></div>
+    </div>
 </template>
 
 <script setup lang='ts'>
 import { onMounted } from "vue";
-import { Editor, withUndoRedo } from "web-editor-markdown";
+import { Editor, withUndoRedo } from "@/editor/index";
+
+import { ArrowBackSharp } from '@vicons/ionicons5';
+import { Edit16Filled } from '@vicons/fluent'
 
 onMounted(() => {
     let editor = new Editor(document.getElementById('main') as HTMLElement);
@@ -19,13 +41,79 @@ onMounted(() => {
 
 <style scoped>
 
-#main {
+.container {
+    width: 100%;
     height: 100%;
     display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.header {
+    width: 100%;
+    height: 50px;
+    background-color: #ddd;
+    display:flex;
     justify-content: center;
 }
 
-.web-editor-pre {
-    min-height: 200px !important;
+.headerContainer {
+    width:70%;
+    height: 100%;
+    display:flex;
+}
+
+.headerLeft {
+    height: 100%;
+    width: 50%;
+    display: flex;
+    align-items: center;
+}
+
+.back{
+    font-size: 0;
+    margin-left: 10px;
+}
+.back:hover{
+    cursor: pointer;
+}
+
+.title {
+    font-size: 20px;
+    font-weight: 666;
+    margin-left: 10px;
+}
+
+.editTitle {
+    font-size: 0;
+    margin-left: 4px;
+    opacity: 0.5;
+}
+
+.editTitle:hover{
+    cursor: pointer;
+    opacity: 1;
+    transition: 0.1s opacity linear
+}
+
+.headerRight {
+    height: 100%;
+    width: 50%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
+
+.headerRight > * {
+    margin-right:10px;
+}
+
+#main {
+    width: 70%;
+    height: calc(100% - 50px);
+    display: flex;
+    justify-content: center;
+    margin-top: 10px;
+    margin-bottom: 5px;
 }
 </style>
