aa81b99e162c52c0a0cb4e19c74795d33b4229aa
Nat2Y <769468283@qq.com>
2023-08-31 15:45:30 +0800

    feat:增加项目回收站功能

--- src/pages/Team/ProjectManage.vue
+++ src/pages/Team/ProjectManage.vue
@@ -28,7 +28,20 @@
                         </div>
                     </n-tab-pane>
                     <n-tab-pane name="rubbish bin" tab="回收站">
-                        回收站
+                        <div class="delete-project">
+                            <n-input 
+                            class="delete-search" 
+                            placeholder="输入搜索项目" 
+                            clearable
+                            @input="filtProject"
+                            >
+                                <template #prefix>
+                                    <n-icon :component="BoxSearch20Filled" />
+                                </template>
+                            </n-input>
+                            <n-data-table :key="(row: any) => row.key" :columns="deleteColumns" :data="preDeleteData"
+                                :pagination="deletePaginationRef" @update:deletePage="handleDeletePageChange" />
+                        </div>
                     </n-tab-pane>
                 </n-tabs>
 
@@ -54,6 +67,8 @@ const route = useRoute()
 const tid = ref(route.params.tid.toString())
 const data: Ref<ProjectRowData[]> = ref([])
 const preData: Ref<ProjectRowData[]> = ref([])
+const deleteData: Ref<ProjectRowData[]> = ref([])
+const preDeleteData: Ref<ProjectRowData[]> = ref([])
 
 // 前后端逻辑
 const projectStore = useProjectStore();
@@ -68,6 +83,7 @@ import DeleteConfirm from '@/components/Project/DeleteConfirm.vue'
 import router from '@/routes';
 const projectNameInputRef = ref()
 const projectDescriptionInputRef = ref()
+
 const columns = [
     {
         title: '项目名称',
@@ -177,7 +193,7 @@ const columns = [
         render(row: ProjectRowData) {
             return h('div', {
                 style: 'min-height: 22px',
-            }, row.creatTime)
+            }, row.creatTime.substring(0, 16))
         }
     },
     {
@@ -204,6 +220,103 @@ const columns = [
     }
 ]
 
+const deleteColumns = [
+    {
+        title: '项目名称',
+        key: 'projectname',
+        width: 180,
+        sorter(rowA: ProjectRowData, rowB: ProjectRowData) {
+            return rowA.projectname.localeCompare(rowB.projectname)
+        },
+        render(row: ProjectRowData) {
+            return h('div', {
+                    style: 'min-height: 22px',
+                    onClick: () => {
+                        row.isEditing = true
+                        nextTick(() => {
+                            projectNameInputRef.value.focus()
+                        })
+                    }
+                }, row.projectname)
+        }
+    },
+    {
+        title: '描述',
+        key: 'description',
+        width: 250,
+        render(row: ProjectRowData) {
+            return h('div', {
+                    style: 'min-height: 22px',
+                    onClick: () => {
+                        row.isEditing = true
+                        nextTick(() => {
+                            projectDescriptionInputRef.value.focus()
+                        })
+                    }
+                }, row.description)
+        }
+    },
+    {
+        title: '创建者',
+        key: 'creator',
+        width: 130,
+        render(row: ProjectRowData) {
+            return h('div', {
+                style: 'min-height: 22px',
+            }, row.creator)
+        }
+    },
+    {
+        title: '创建时间',
+        key: 'creatTime',
+        width: 230,
+        sorter(rowA: ProjectRowData, rowB: ProjectRowData) {
+            const dateA = new Date(rowA.creatTime.replace(' ', 'T'));
+            const dateB = new Date(rowB.creatTime.replace(' ', 'T'));
+            return dateA.getTime() - dateB.getTime()
+        },
+        render(row: ProjectRowData) {
+            return h('div', {
+                style: 'min-height: 22px',
+            }, row.creatTime.substring(0, 16))
+        }
+    },
+    {
+        title: '',
+        key: 'option',
+        render(row: ProjectRowData) {
+            return h('div', {
+                style: "display:flex"
+            }, [
+                h(DeleteConfirm, {
+                    buttonText: '删除',
+                    id: row.key,
+                }),
+                h(NButton, {
+                    style: "margin-left: 10px",
+                    onclick: () => {
+                        const tid = row.key.split('.')[0]
+                        const pid = row.key.split('.')[1]
+                        axios.post('project/recoverProject', {
+                            pid : pid,
+                            tid : tid
+                        }).then(res => {
+                            if (res.status === 200) {
+                                if (res.data.code === 200) {
+                                    message.success('恢复成功')
+                                    projectstore.projectChanged.value = !projectstore.projectChanged.value
+                                } else {
+                                    message.warning(res.data.message)
+                                }
+                            }
+                        })
+                    }
+                }, "恢复")
+            ]);
+        }
+    }
+]
+
 // filter
 function filtProject(v: string) {
     if (!v) {
@@ -222,6 +335,16 @@ const paginationRef: Ref<{ pageSize: number; page: number }> = computed(() => ({
     pageSize: 6,
     page: page.value
 }))
+
+const deletePage: Ref<number> = ref(1)
+const handleDeletePageChange = (curPage: number): void => {
+    deletePage.value = curPage
+}
+const deletePaginationRef: Ref<{ pageSize: number; page: number }> = computed(() => ({
+    pageSize: 6,
+    page: page.value
+}))
+
 
 
 const refreshData = () => {
@@ -242,6 +365,30 @@ const refreshData = () => {
                 })
                 preData.value = data.value
                 page.value = 1
+            } else {
+                message.warning(res.data.message)
+            }
+        } else {
+            message.error('服务器错误')
+        }
+    })
+    axios.post('project/viewDelProject', {
+        tid: tid.value.replace('private', '')
+    }).then(res => {
+        if (res.status === 200) {
+            if (res.data.code === 200) {
+                deleteData.value = res.data.data.projectlist?.map((item: any) => {
+                    return {
+                        key: tid.value.replace('private', '') + '.' + item.pid,   // pid
+                        projectname: item.project_name,
+                        description: item.project_inform,
+                        creator: item.username,
+                        creatTime: item.create_time.split('.')[0].replace('T', ' '),
+                        isEditing: false,
+                    }
+                })
+                preDeleteData.value = deleteData.value
+                deletePage.value = 1
             } else {
                 message.warning(res.data.message)
             }
@@ -278,7 +425,6 @@ onMounted(() => {
 }
 
 .project-bottom {
-
     display: inline-flex;
     flex-direction: row;
 }
@@ -291,6 +437,14 @@ onMounted(() => {
     margin: 10px;
 
     .active-search {
+        margin-bottom: 10px;
+    }
+}
+
+.delete-project {
+    margin: 10px;
+
+    .delete-search {
         margin-bottom: 10px;
     }
 }
